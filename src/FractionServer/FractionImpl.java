176643bfa569ac7e474429d0186eb2e93a573fc3
mikedalpee <mikedalpee@gmail.com>
2017-10-21 17:17:17 -0400

    Clean up a little bit

--- src/FractionServer/FractionImpl.java
+++ src/FractionServer/FractionImpl.java
@@ -3,10 +3,16 @@ import FractionIDL.*;
 import com.softmoore.math.Fraction;
 
 public class FractionImpl extends FractionPOA{
+    private static String toString(FractionRep fr)
+    {
+        return fr.numerator+"/"+fr.denominator;
+    }
     @Override
     public FractionRep add(FractionRep lhs, FractionRep rhs){
         com.softmoore.math.Fraction f = new Fraction(lhs.numerator,lhs.denominator);
-        f.add(new Fraction(rhs.numerator,rhs.denominator));
-        return new FractionRep(f.getNumerator(),f.getDenominator());
+        Fraction res = f.add(new Fraction(rhs.numerator,rhs.denominator));
+        FractionRep rep = new FractionRep(res.getNumerator(),res.getDenominator());
+        System.out.println(toString(lhs)+" + "+toString(rhs)+" = "+toString(rep));
+        return rep;
     }
 }
